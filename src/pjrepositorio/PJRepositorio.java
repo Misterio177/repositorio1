7a2535e0c86764f486882b557c1fa1c034546511
Guillermo <Guillermo@DESKTOP-G00R2KV>
2021-05-30 17:20:37 -0500

    Profesor quiero mi 20

--- src/pjrepositorio/PJRepositorio.java
+++ src/pjrepositorio/PJRepositorio.java
@@ -24,6 +24,7 @@ public class PJRepositorio {
         System.out.println("La cosa solo es tener amigos para compartir tu trabajo");
         System.out.println("La cosa solo es tener amigos para compartir tu trabajo");
         System.out.println("La cosa solo es tener amigos para compartir tu trabajo");
+        System.out.println("La cosa solo es tener amigos para compartir tu trabajo");
     }
     
 }
