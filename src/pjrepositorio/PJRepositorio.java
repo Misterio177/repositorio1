5e743d9e42a6e6a285f8d9b269c896d87fa61fcb
Guillermo <Guillermo@DESKTOP-G00R2KV>
2021-05-30 15:59:06 -0500

    Profesor quiero mi 20

--- src/pjrepositorio/PJRepositorio.java
+++ src/pjrepositorio/PJRepositorio.java
@@ -16,6 +16,7 @@ public class PJRepositorio {
      */
     public static void main(String[] args) {
         System.out.println("Hola Mundo");
+        System.out.println("Hola Edmundo");
     }
     
 }
